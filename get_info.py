240c8fb0e4fd68e50c124105201c3504f7fd4a8b
Kevin Zihlmann <kevin.zihlmann@bluewin.ch>
2022-09-17 07:48:17 +0800

    bugfix

--- get_info.py
+++ get_info.py
@@ -5,8 +5,8 @@ def get_info():
     # Station Reuss - Mellingen - 2018
     html_text = requests.get("https://www.hydrodaten.admin.ch/de/2018.html").text
     soup = BeautifulSoup(html_text, 'lxml')
-    table = soup.find('table', class_= 'table table-bordered table-narrow')
+    table = soup.find('table'ß, class_= 'table table-bordered table-narrow')
     time = table.find('small', class_='text-muted').text
     abfluss = table.find('td', class_='text-center').text
     
-    return time, abflussß
+    return time, abfluss
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from keep_alive import keep_alive
+from get_info import get_info
 
 import time
 
@@ -6,12 +7,16 @@ keep_alive()
 
 while True:
   time.sleep(15)
+  t, abfluss = get_info()
+
+
   f = open("time.txt", "a")
   f.write(time.asctime())
   f.write("\n")
   f.close()
   f2 = open("abfluss_mellingen.txt", "a")
-  f2.write(time.asctime())
+  f2.write(t)
   f2.write(" ")
-  f2.write(str(get_info()))
+  f2.write(abfluss)
+  f2.write("\n")
   f2.close()
