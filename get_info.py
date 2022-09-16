31c5a632f7ce0c8db4726ba6b9c81b33bee840cb
Kevin Zihlmann <kevin.zihlmann@bluewin.ch>
2022-09-17 07:40:34 +0800

    to be tested on replit.com

---
+++ get_info.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+def get_info():
+    # Station Reuss - Mellingen - 2018
+    html_text = requests.get("https://www.hydrodaten.admin.ch/de/2018.html").text
+    soup = BeautifulSoup(html_text, 'lxml')
+    table = soup.find('table', class_= 'table table-bordered table-narrow')
+    time = table.find('small', class_='text-muted').text
+    abfluss = table.find('td', class_='text-center').text
+    
+    return time, abflussß
--- main.py
+++ main.py
@@ -5,8 +5,13 @@ import time
 keep_alive()
 
 while True:
-  time.sleep(5)
+  time.sleep(15)
   f = open("time.txt", "a")
   f.write(time.asctime())
   f.write("\n")
   f.close()
+  f2 = open("abfluss_mellingen.txt", "a")
+  f2.write(time.asctime())
+  f2.write(" ")
+  f2.write(str(get_info()))
+  f2.close()
