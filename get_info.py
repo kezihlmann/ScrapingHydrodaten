d370837b607a13c6405a9c89658cc233525b46de
Kevin Zihlmann <kevin.zihlmann@bluewin.ch>
2022-09-17 07:50:29 +0800

    bugfix

--- get_info.py
+++ get_info.py
@@ -5,7 +5,7 @@ def get_info():
     # Station Reuss - Mellingen - 2018
     html_text = requests.get("https://www.hydrodaten.admin.ch/de/2018.html").text
     soup = BeautifulSoup(html_text, 'lxml')
-    table = soup.find('table'ß, class_= 'table table-bordered table-narrow')
+    table = soup.find('table', class_= 'table table-bordered table-narrow')
     time = table.find('small', class_='text-muted').text
     abfluss = table.find('td', class_='text-center').text
     
